a033f194777500480ec22484618b093a15c24ade
Mario Amaya <thefozcat@gmail.com>
2019-04-11 12:11:01 -0700

    first commit

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
